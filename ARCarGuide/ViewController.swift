bbef473c18490d2582599f8e8d94a33ebec3edc3
eldare <eldar@healthy.io>
2020-09-04 13:52:24 +0300

    Clean ARKit project

---
+++ ARCarGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARCarGuide
+//
+//  Created by Eldar Eliav on 04/09/2020.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
