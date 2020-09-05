c992f6bb6b4d23fa98f699bacd81285ba2c4eddb
eldare <eldar@healthy.io>
2020-09-05 23:43:32 +0300

    Logger - a simple info/error print wrapper

---
+++ ARCarGuide/Logger.swift
@@ -0,0 +1,19 @@
+//
+//  Logger.swift
+//  ARCarGuide
+//
+//  Created by Eldar Eliav on 04/09/2020.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import Foundation
+
+struct log {
+    static func info(_ text: String) {
+        print("[INFO] \(text)")
+    }
+
+    static func error(_ text: String) {
+        print("[ERROR] \(text)")
+    }
+}
